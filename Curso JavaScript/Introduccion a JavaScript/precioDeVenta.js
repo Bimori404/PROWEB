8becee26846450a7dfc72d20a687272fffe46401
/Bimori <106877704+Bimori404@users.noreply.github.com>
2024-07-02 11:33:33 -0600

    Se va crear un proyecto aparte

--- Curso JavaScript/Introduccion a JavaScript/precioDeVenta.js
+++
@@ -1,9 +0,0 @@
-var precio = 10;
-var producto = 4;
-
-console.log("El precio del producto es "+precio);
-console.log("La cantidad de productos que estas comprando son "+producto);
-
-var resultado = precio*producto;
-
-console.log("El precio a pagar es el siguiente: "+resultado)
--- Curso JavaScript/Introduccion a JavaScript/saludar.js
+++
@@ -1,8 +0,0 @@
-var nombre = "matias"
-
-function saludar (nombre) {
-    console.log("Hola "+nombre);
-}
-
-// saludar("Samuel");
-saludar(nombre);
